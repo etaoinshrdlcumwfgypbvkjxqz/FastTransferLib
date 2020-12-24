fff5205026ecf4b9aafd436023c326236f34e701
William So <williamso26144225@gmail.com>
2020-12-24 14:02:14 +0800

    Create UnspecifiedModel
    Signed-off-by: William So <williamso26144225@gmail.com>

---
+++ src/main/java/dev/technici4n/fasttransferlib/impl/base/UnspecifiedModel.java
@@ -0,0 +1,8 @@
+package dev.technici4n.fasttransferlib.impl.base;
+
+import dev.technici4n.fasttransferlib.api.view.model.Model;
+
+public enum UnspecifiedModel
+        implements Model {
+    INSTANCE,
+}
